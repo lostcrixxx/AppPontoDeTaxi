e0d21be879df2457e3c1dcc29e41f5e0ae3f4cf0
Cristiano Morais <devcristianomorais@gmail.com>
2019-09-02 06:03:36 +0900

    back button in action bar

--- app/src/main/java/br/com/cristianomoraiscruz/pontodetaxi/MainActivity.java
+++ app/src/main/java/br/com/cristianomoraiscruz/pontodetaxi/MainActivity.java
@@ -1,14 +1,12 @@
 package br.com.cristianomoraiscruz.pontodetaxi;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
