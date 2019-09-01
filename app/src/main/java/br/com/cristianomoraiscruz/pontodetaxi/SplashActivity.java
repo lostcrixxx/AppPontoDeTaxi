7f578c87ba8ce6d7c9cd5bf69bdf97e1ffbee126
Cristiano Morais <devcristianomorais@gmail.com>
2019-09-02 04:34:35 +0900

    ädding custom menu layout

---
+++ app/src/main/java/br/com/cristianomoraiscruz/pontodetaxi/SplashActivity.java
@@ -0,0 +1,28 @@
+package br.com.cristianomoraiscruz.pontodetaxi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final static int TIME_SPLASH = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent dashboard = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(dashboard);
+                finish();
+
+            }
+        },TIME_SPLASH);
+    }
+}
