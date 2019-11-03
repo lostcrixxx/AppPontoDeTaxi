ae7207520dc361cdf3e6987f65b412a85fc03db1
Cristiano Morais <devcristianomorais@gmail.com>
2019-11-03 20:44:40 +0900

    versionName

--- app/src/main/java/br/com/cristianomoraiscruz/pontodetaxi/view/SobreActivity.java
+++ app/src/main/java/br/com/cristianomoraiscruz/pontodetaxi/view/SobreActivity.java
@@ -1,16 +1,26 @@
 package br.com.cristianomoraiscruz.pontodetaxi.view;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import br.com.cristianomoraiscruz.pontodetaxi.BuildConfig;
 import br.com.cristianomoraiscruz.pontodetaxi.R;
 
 public class SobreActivity extends AppCompatActivity {
+
+    String versionName;
+    TextView txtVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
 
+        txtVersion = findViewById(R.id.txtVersion);
+        versionName = BuildConfig.VERSION_NAME;
+
+        txtVersion.setText(versionName);
     }
 }
