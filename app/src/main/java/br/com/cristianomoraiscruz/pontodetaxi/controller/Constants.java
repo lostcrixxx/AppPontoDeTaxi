bf430802ca03249636eaf19fb7d92547389aebd9
Cristiano Morais <devcristianomorais@gmail.com>
2019-11-04 10:59:23 +0900

    mask taxista

---
+++ app/src/main/java/br/com/cristianomoraiscruz/pontodetaxi/controller/Constants.java
@@ -0,0 +1,8 @@
+package br.com.cristianomoraiscruz.pontodetaxi.controller;
+
+public class Constants {
+
+    public static final String MASK_PHONE = "99 9 9999 9999";
+    public static final String MASK_DATE = "99 99 9999";
+    public static final String MASK_PLAQUE = "AAA 9999";
+}
